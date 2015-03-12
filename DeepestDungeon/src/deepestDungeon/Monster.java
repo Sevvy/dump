a0f6a9bb890a6d37ff0da4691259e5c85b4521d4
曾学文 <JonXCh@gmail.com>
2015-03-12 15:23:24 +1100

    Create Monster.java

---
+++ DeepestDungeon/src/deepestDungeon/Monster.java
@@ -0,0 +1,21 @@
+package deepestDungeon;
+
+public class Monster extends Entity {
+
+	public Monster(int hp, int energy){
+		super(hp,energy);
+	}
+
+	@Override
+	public void getResponse() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDeath() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

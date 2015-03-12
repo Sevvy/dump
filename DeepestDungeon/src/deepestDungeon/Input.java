e3662976c20f0833019a9c038e31d7b3dca1550f
曾学文 <JonXCh@gmail.com>
2015-03-12 15:22:38 +1100

    Create Input.java

---
+++ DeepestDungeon/src/deepestDungeon/Input.java
@@ -0,0 +1,41 @@
+package deepestDungeon;
+
+import java.util.Scanner;
+
+public abstract class Input {
+	private static Scanner sc = new Scanner(System.in);
+	private Input(){};
+	public static String getNext(){
+		return sc.nextLine();
+	}
+
+	public static int getNextInt(boolean manualCheck){ //only accepts ints >= 0, returns -1 if invalid int, -2 if invalid input.
+		while(true){
+			String s = sc.nextLine();
+			int i = -2;
+			try{
+				i = Integer.parseInt(s);
+			}
+			catch(Exception e){
+				if(manualCheck){
+					return -2;
+				}
+				System.out.println("Please input a valid number.");
+				continue;
+			}
+			if(i < 0){
+				if(manualCheck){
+					return -1;
+				}
+				System.out.println("Please input a number greater than -1.");
+				continue;
+			}
+			return i;
+		}
+	}
+	
+	public static int getNextInt(){
+		return getNextInt(false);
+	}
+
+}

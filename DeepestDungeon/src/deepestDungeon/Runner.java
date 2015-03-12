f8e5cbc5776e8a6ddeb019399f84cb538e616e36
曾学文 <JonXCh@gmail.com>
2015-03-12 15:24:22 +1100

    Create Runner.java

---
+++ DeepestDungeon/src/deepestDungeon/Runner.java
@@ -0,0 +1,16 @@
+package deepestDungeon;
+
+public class Runner {
+
+	public Runner() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		for(int i = 0; i<1000; i++){
+			System.out.println(Mathz.random(0, 10));
+		}
+
+	}
+
+}
